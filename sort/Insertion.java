fa503d1143cd350b4451a91457d89947efbc68e9
112358mw <874021127@qq.com>
2018-03-28 16:42:20 +0800

    插入排序

---
+++ sort/Insertion.java
@@ -0,0 +1,12 @@
+public class Insertion {
+    public static void sort(Comparable[] a) {
+        //将a[]按升序排列
+        int N = a.length;
+        for(int i = 1; i < N; i++){
+            //将a[i]插入到a[i-1], a[i-2], a[i-3]...中
+            for (int j = i; j > 0 && less(a[j], a[j-1]); j--)  {
+                    exch(a, j, j-1);
+            }
+        }
+    }
+}
